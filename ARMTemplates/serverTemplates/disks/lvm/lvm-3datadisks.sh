bc737345b46bc18ca085ef8c09be60dc7a066b49
Kimmo Forss <kimmo.forss@microsoft.com>
2020-03-09 01:06:00 +0200

    fixed disk mapping

--- ARMTemplates/serverTemplates/disks/lvm/lvm-3datadisks.sh
+++ ARMTemplates/serverTemplates/disks/lvm/lvm-3datadisks.sh
@@ -1,8 +1,8 @@
+sudo pvcreate /dev/sdc
+sudo pvcreate /dev/sdd
 sudo pvcreate /dev/sde
-sudo pvcreate /dev/sdf
-sudo pvcreate /dev/sdg
 
-sudo vgcreate data-vg01 /dev/sde /dev/sdf /dev/sdg
+sudo vgcreate data-vg01 /dev/sdc /dev/sdd /dev/sde
 sudo lvcreate --extents 100%FREE --stripes 3 --name data-lv01 data-vg01
 sudo mkfs -t ext4 /dev/data-vg01/data-lv01
 sudo mkdir /data
--- ARMTemplates/serverTemplates/disks/lvm/lvm-5datadisks2logdisks.sh
+++ ARMTemplates/serverTemplates/disks/lvm/lvm-5datadisks2logdisks.sh
@@ -1,20 +1,18 @@
+sudo pvcreate /dev/sdc
+sudo pvcreate /dev/sdd
 sudo pvcreate /dev/sde
 sudo pvcreate /dev/sdf
 sudo pvcreate /dev/sdg
-sudo pvcreate /dev/sdh
-sudo pvcreate /dev/sdi
 
-sudo vgcreate data-vg01 /dev/sde /dev/sdf /dev/sdg /dev/sdh /dev/sdi
+sudo vgcreate data-vg01 /dev/sdc /dev/sdd /dev/sde /dev/sdf /dev/sdg
 sudo lvcreate --extents 100%FREE --stripes 5 --name data-lv01 data-vg01
-sudo mkfs -t ext4 /dev/data-vg01/data-lv01
-sudo mkdir /data
 
 echo "/dev/data-vg01/data-lv01  /data  ext4  defaults, barrier=0  0  2" | sudo tee -a /etc/fstab
 
-sudo pvcreate /dev/sdj
-sudo pvcreate /dev/sdk
+sudo pvcreate /dev/sdh
+sudo pvcreate /dev/sdi
 
-sudo vgcreate log-vg01 /dev/sdj /dev/sdk
+sudo vgcreate log-vg01 /dev/sdh /dev/sdi
 sudo lvcreate --extents 100%FREE --stripes 2 --name log-lv01 log-vg01
 sudo mkfs -t ext4 /dev/log-vg01/log-lv01
 sudo mkdir /log
