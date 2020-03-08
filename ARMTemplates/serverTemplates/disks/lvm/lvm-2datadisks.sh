593c7078630ea367c9eb3d8356daa4b978abebae
Kimmo Forss <kimmo.forss@microsoft.com>
2020-03-09 01:01:08 +0200

    Update lvm-2datadisks.sh

--- ARMTemplates/serverTemplates/disks/lvm/lvm-2datadisks.sh
+++ ARMTemplates/serverTemplates/disks/lvm/lvm-2datadisks.sh
@@ -1,7 +1,7 @@
 sudo pvcreate /dev/sdc
 sudo pvcreate /dev/sdd
 
-sudo vgcreate data-vg01 /dev/sdc /dev/sds
+sudo vgcreate data-vg01 /dev/sdc /dev/sdd
 sudo lvcreate --extents 100%FREE --stripes 3 --name data-lv01 data-vg01
 sudo mkfs -t ext4 /dev/data-vg01/data-lv01
 sudo mkdir /data
