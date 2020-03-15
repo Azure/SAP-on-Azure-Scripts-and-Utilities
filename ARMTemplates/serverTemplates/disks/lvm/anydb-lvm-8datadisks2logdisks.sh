a15778b5c83127ee8b4fb5e041b6045b9bc3a17e
Kimmo Forss <kimmo.forss@microsoft.com>
2020-03-15 15:34:57 +0200

    fixed formatting bug

--- ARMTemplates/serverTemplates/disks/lvm/anydb-lvm-8datadisks2logdisks.sh
+++ ARMTemplates/serverTemplates/disks/lvm/anydb-lvm-8datadisks2logdisks.sh
@@ -10,7 +10,9 @@ sudo pvcreate /dev/sdj
 sudo vgcreate data-vg01 /dev/sdc /dev/sdd /dev/sde /dev/sdf /dev/sdg /dev/sdh /dev/sdi /dev/sdj
 sudo lvcreate --extents 100%FREE --stripes 8 --name data-lv01 data-vg01
 
+sudo mkfs -t ext4 /dev/data-vg01/data-lv01
 sudo mkdir /data
+
 echo "/dev/data-vg01/data-lv01  /data  ext4  defaults,barrier=0,nofail  0  2" | sudo tee -a /etc/fstab
 
 sudo pvcreate /dev/sdk
