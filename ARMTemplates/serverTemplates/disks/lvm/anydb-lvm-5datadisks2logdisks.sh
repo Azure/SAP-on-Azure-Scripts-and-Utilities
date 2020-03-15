3531859a9065543e11dcafc0e9abf6a3e3534326
Kimmo Forss <kimmo.forss@microsoft.com>
2020-03-15 14:52:50 +0200

    fixed missing data directory in lvm script

--- ARMTemplates/serverTemplates/disks/lvm/anydb-lvm-5datadisks2logdisks.sh
+++ ARMTemplates/serverTemplates/disks/lvm/anydb-lvm-5datadisks2logdisks.sh
@@ -7,6 +7,7 @@ sudo pvcreate /dev/sdg
 sudo vgcreate data-vg01 /dev/sdc /dev/sdd /dev/sde /dev/sdf /dev/sdg
 sudo lvcreate --extents 100%FREE --stripes 5 --name data-lv01 data-vg01
 
+sudo mkdir /data
 echo "/dev/data-vg01/data-lv01  /data  ext4  defaults,barrier=0,nofail  0  2" | sudo tee -a /etc/fstab
 
 sudo pvcreate /dev/sdh
--- ARMTemplates/serverTemplates/disks/lvm/anydb-lvm-8datadisks2logdisks.sh
+++ ARMTemplates/serverTemplates/disks/lvm/anydb-lvm-8datadisks2logdisks.sh
@@ -10,6 +10,7 @@ sudo pvcreate /dev/sdj
 sudo vgcreate data-vg01 /dev/sdc /dev/sdd /dev/sde /dev/sdf /dev/sdg /dev/sdh /dev/sdi /dev/sdj
 sudo lvcreate --extents 100%FREE --stripes 8 --name data-lv01 data-vg01
 
+sudo mkdir /data
 echo "/dev/data-vg01/data-lv01  /data  ext4  defaults,barrier=0,nofail  0  2" | sudo tee -a /etc/fstab
 
 sudo pvcreate /dev/sdk
