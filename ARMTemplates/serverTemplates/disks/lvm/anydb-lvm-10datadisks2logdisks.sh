454afe45a369fab2337ce63a71687240676aa690
Kimmo Forss <kimmo.forss@microsoft.com>
2020-03-15 10:59:31 +0200

    Support for larger anydb sizes

---
+++ ARMTemplates/serverTemplates/disks/lvm/anydb-lvm-10datadisks2logdisks.sh
@@ -0,0 +1,27 @@
+sudo pvcreate /dev/sdc
+sudo pvcreate /dev/sdd
+sudo pvcreate /dev/sde
+sudo pvcreate /dev/sdf
+sudo pvcreate /dev/sdg
+sudo pvcreate /dev/sdh
+sudo pvcreate /dev/sdi
+sudo pvcreate /dev/sdj
+sudo pvcreate /dev/sdk
+sudo pvcreate /dev/sdl
+
+sudo vgcreate data-vg01 /dev/sdc /dev/sdd /dev/sde /dev/sdf /dev/sdg /dev/sdh /dev/sdi /dev/sdj /dev/sdk /dev/sdl
+sudo lvcreate --extents 100%FREE --stripes 10 --name data-lv01 data-vg01
+
+echo "/dev/data-vg01/data-lv01  /data  ext4  defaults,barrier=0,nofail  0  2" | sudo tee -a /etc/fstab
+
+sudo pvcreate /dev/sdm
+sudo pvcreate /dev/sdn
+
+sudo vgcreate log-vg01 /dev/sdm /dev/sdn
+sudo lvcreate --extents 100%FREE --stripes 2 --name log-lv01 log-vg01
+sudo mkfs -t ext4 /dev/log-vg01/log-lv01
+sudo mkdir /log
+
+echo "/dev/log-vg01/log-lv01  /log  ext4  defaults,barrier=0,nofail  0  2" | sudo tee -a /etc/fstab
+
+
---
+++ ARMTemplates/serverTemplates/disks/lvm/anydb-lvm-13datadisks3logdisks.sh
@@ -0,0 +1,31 @@
+sudo pvcreate /dev/sdc
+sudo pvcreate /dev/sdd
+sudo pvcreate /dev/sde
+sudo pvcreate /dev/sdf
+sudo pvcreate /dev/sdg
+sudo pvcreate /dev/sdh
+sudo pvcreate /dev/sdi
+sudo pvcreate /dev/sdj
+sudo pvcreate /dev/sdk
+sudo pvcreate /dev/sdl
+sudo pvcreate /dev/sdm
+sudo pvcreate /dev/sdn
+sudo pvcreate /dev/sdo
+
+sudo vgcreate data-vg01 /dev/sdc /dev/sdd /dev/sde /dev/sdf /dev/sdg /dev/sdh /dev/sdi /dev/sdj /dev/sdk /dev/sdl /dev/sdm /dev/sdn /dev/sdn
+sudo lvcreate --extents 100%FREE --stripes 13 --name data-lv01 data-vg01
+
+echo "/dev/data-vg01/data-lv01  /data  ext4  defaults,barrier=0,nofail  0  2" | sudo tee -a /etc/fstab
+
+sudo pvcreate /dev/sdp
+sudo pvcreate /dev/sdq
+sudo pvcreate /dev/sdr
+
+sudo vgcreate log-vg01 /dev/sdp /dev/sdq /dev/sdr
+sudo lvcreate --extents 100%FREE --stripes 3 --name log-lv01 log-vg01
+sudo mkfs -t ext4 /dev/log-vg01/log-lv01
+sudo mkdir /log
+
+echo "/dev/log-vg01/log-lv01  /log  ext4  defaults,barrier=0,nofail  0  2" | sudo tee -a /etc/fstab
+
+
