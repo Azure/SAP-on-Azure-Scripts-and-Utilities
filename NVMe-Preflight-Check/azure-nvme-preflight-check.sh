e0d4f5dc117564435d298c1a2db1a2d3e9d195c2
Philipp Leitenbauer <37079522+msftphleiten@users.noreply.github.com>
2024-02-22 17:30:11 +0100

    Update azure-nvme-preflight-check.sh

--- NVMe-Preflight-Check/azure-nvme-preflight-check.sh
+++ NVMe-Preflight-Check/azure-nvme-preflight-check.sh
@@ -44,11 +44,15 @@ check_NVMe_initrd () {
         lsinitrd /boot/initramfs-$(uname -r).img|grep nvme > /dev/null 2>&1
         if [ $? -ne 0 ]; then
             # NVMe module is not loaded in initrd/initramfs
+            echo -e "------------------------------------------------"
             echo -e "ERROR  NVMe Module is not loaded in the initramfs image."
             echo -e "\t- Please run the following command on your instance to recreate initramfs:"
             echo -e '\t# sudo dracut -f -v'
+            echo -e "------------------------------------------------"
         else
+            echo -e "------------------------------------------------"
             echo -e "OK     NVMe Module loaded in initramfs image."
+            echo -e "------------------------------------------------"
         fi
     
     elif [[ "${find_distro}" == "sles" ]] ; then
@@ -56,11 +60,19 @@ check_NVMe_initrd () {
         lsinitrd /boot/initrd-$(uname -r)|grep nvme > /dev/null 2>&1
         if [ $? -ne 0 ]; then
             # NVMe module is not loaded in initrd/initramfs
+            echo -e "------------------------------------------------"
             echo -e "ERROR  NVMe Module is not loaded in the initramfs image."
-            echo -e "\t- Please run the following command on your instance to recreate initramfs:"
-            echo -e '\t# sudo dracut -f -v'
+            echo -e ""
+            echo -e "\tmkdir -p /etc/dracut.conf.d"
+            echo -e "\techo 'add_drivers+=\" nvme nvme-core nvme-fabrics nvme-fc nvme-rdma nvme-loop nvmet nvmet-fc nvme-tcp \"' >/etc/dracut.conf.d/nvme.conf"
+            echo -e '\tdracut -f -v'
+            echo -e ""
+            echo -e "------------------------------------------------"
         else
+            # NVMe module is loaded into initramfs
+            echo -e "------------------------------------------------"
             echo -e "OK     NVMe Module loaded in initramfs image."
+            echo -e "------------------------------------------------"
         fi
         
     elif [ -f /etc/debian_version ] ; then
@@ -68,11 +80,17 @@ check_NVMe_initrd () {
         lsinitramfs /boot/initrd.img-$(uname -r)|grep nvme > /dev/null 2>&1
         if [ $? -ne 0 ]; then
             # NVMe module is not loaded in initrd/initramfs
+            echo -e "------------------------------------------------"
             echo -e "ERROR  NVMe Module is not loaded in the initramfs image."
             echo -e "\t- Please run the following command on your instance to recreate initramfs:"
             echo -e '\t# sudo update-initramfs -c -k all'
+            echo -e "------------------------------------------------"
+
         else
+            echo -e "------------------------------------------------"
             echo -e "OK     NVMe Module loaded in initramfs image."
+            echo -e "------------------------------------------------"
+
         fi
 
     else 
@@ -86,6 +104,62 @@ check_NVMe_initrd () {
 ########################################################################
 # END function check_NVME_initrd
 ########################################################################
+
+########################################################################
+# START function check_grub
+#
+# function checks if grub has the nvme_core.iotimeout configured
+#
+# TODO add more distributions like
+# - Oracle Linux
+# - Almalinux
+#
+########################################################################
+check_grub_nvme_timeout () {
+
+    find_distro=`cat /etc/os-release |sed -n 's|^ID="\([a-z]\{4\}\).*|\1|p'`  
+
+    if [ -f /etc/redhat-release ] ; then
+        # add RH code
+        echo ""
+
+    elif [[ "${find_distro}" == "sles" ]] ; then
+        # Distribution is SUSE Linux
+        cat /etc/default/grub | grep "GRUB_CMDLINE_LINUX" | grep "nvme_core.io_timeout=240"
+        if [ $? -ne 0 ]; then
+            # GRUB configuration missing
+            echo -e "------------------------------------------------"
+            echo -e "ERROR  Please add parameters to GRUB_CMDLINE_LINUX"
+            echo -e ""
+            echo -e '\tnvme_core.io_timeout=240 nvme_core.admin_timeout=240'
+            echo -e ""
+            echo -e "\tit should then look something like this:"
+            echo -e "\tGRUB_CMDLINE_LINUX_DEFAULT=""console=ttyS0 net.ifnames=0 dis_ucode_ldr earlyprintk=ttyS0 multipath=off rootdelay=300 scsi_mod.use_blk_mq=1 USE_BY_UUID_DEVICE_NAMES=1 nvme_core.io_timeout=240 nvme_core.admin_timeout=240"""
+            echo -e ""
+            echo -e "------------------------------------------------"
+        else
+            # NVMe module is loaded into initramfs
+            echo -e "------------------------------------------------"
+            echo -e "OK     GRUB contains timeouts."
+            echo -e "------------------------------------------------"
+        fi
+        
+    elif [ -f /etc/debian_version ] ; then
+        # add debian code
+        echo ""
+
+    else 
+        echo -e "------------------------------------------------"
+        echo -e "ERROR  Unsupported OS for this script."
+        echo -e "------------------------------------------------"
+        exit 1
+    fi
+
+}
+########################################################################
+# END function check_NVME_initrd
+########################################################################
+
 
 
 ########################################################################
@@ -109,9 +183,9 @@ check_fstab () {
     # running replacement for /dev/sd* devices
     
     # Stores all /dev/sd* entries from fstab into a temporary file
-    sed -n 's|^/dev/\(sd[a-z]*[0-9]*\).*|\1|p' </etc/fstab >/tmp/device_names   
+    sed -n 's|^/dev/\(sd[a-z]*[0-9]*\).*|\1|p' </etc/fstab >/tmp/device_names_sd   
     while read LINE; do
-            # For each line in /tmp/device_names
+            # For each line in /tmp/device_names_sd
 
             # get the UUID for the device
             UUID=`ls -l /dev/disk/by-uuid | grep "$LINE" | sed -n 's/^.* \([^ ]*\) -> .*$/\1/p'` # Sets the UUID name for that device
@@ -119,15 +193,15 @@ check_fstab () {
             then
                 sed -i "s|^/dev/${LINE}|UUID=${UUID}|" /etc/fstab.modified.$time_stamp               # Changes the entry in fstab to UUID form
             fi
-    done </tmp/device_names
+    done </tmp/device_names_sd
 
     # running replacement for /dev/disk/azure/scsi1/lun* devices
     
     # Stores all /dev/disk/azure/scsi1/lun* entries from fstab into a temporary file
-    sed -n 's|^/dev/disk/azure/scsi1/\(lun[0-9]*\).*|\1|p' </etc/fstab >/tmp/device_names   
+    sed -n 's|^/dev/disk/azure/scsi1/\(lun[0-9]*\).*|\1|p' </etc/fstab >/tmp/device_names_scsi
 
     while read LINE; do
-            # For each line in /tmp/device_names
+            # For each line in /tmp/device_names_scsi
 
             # first get the real device from azure device
             REALDEVICE=`realpath /dev/disk/azure/scsi1/${LINE} | sed 's+/dev/++g'`
@@ -139,8 +213,10 @@ check_fstab () {
                 # Changes the entry in fstab to UUID form
                 sed -i "s|^/dev/disk/azure/scsi1/${LINE}|UUID=${UUID}|" /etc/fstab.modified.$time_stamp               
             fi
-    done </tmp/device_names
+    done </tmp/device_names_scsi
 
+    cat /tmp/device_names_sd > /tmp/device_names
+    cat /tmp/device_names_scsi >> /tmp/device_names
 
 
     if [ -s /tmp/device_names ]; then
@@ -149,8 +225,9 @@ check_fstab () {
         echo -e "ERROR  Your fstab file contains device names. Mount the partitions using UUID's before changing an instance type to NVMe."
         echo -e "------------------------------------------------"
 
-        printf "Enter y to replace device names with UUID in /etc/fstab file to make it compatible for NVMe block device names.\nEnter n to keep the file as-is with no modification (y/n) "
-        read RESPONSE;
+        # printf "Enter y to replace device names with UUID in /etc/fstab file to make it compatible for NVMe block device names.\nEnter n to keep the file as-is with no modification (y/n) "
+        # read RESPONSE;
+        RESPONSE="n"
         case "$RESPONSE" in
             [yY]|[yY][eE][sS])                                              
                     # If answer is yes, keep the changes to /etc/fstab
@@ -167,24 +244,29 @@ check_fstab () {
                     # don't save the new fstab file
                     echo -e "------------------------------------------------"
                     echo -e "Aborting: Not saving changes..."
+                    echo -e "See proposed changes in /etc/fstab.modified.$time_stamp"
                     rm /etc/fstab.backup.$time_stamp
-                    rm /etc/fstab.modified.$time_stamp
+                    # rm /etc/fstab.modified.$time_stamp
                     echo -e "------------------------------------------------"
                     ;;
             *)                                                              
                     # If answer is anything else, exit and don't save changes
                     # to fstab
                     echo -e "------------------------------------------------"
-                    echo -e "Invalid Response"                                 
+                    echo -e "Invalid Response"       
+                    echo -e "See proposed changes in /etc/fstab.modified.$time_stamp"                          
                     echo -e "Exiting"
                     rm /etc/fstab.backup.$time_stamp
-                    rm /etc/fstab.modified.$time_stamp
+                    #rm /etc/fstab.modified.$time_stamp
                     exit 1
                     echo -e "------------------------------------------------"
                     ;;
     
         esac
         rm /tmp/device_names
+        rm /tmp/device_names_sd
+        rm /tmp/device_names_scsi
+
 
     else 
         rm /etc/fstab.backup.$time_stamp
@@ -244,9 +326,6 @@ if [ $? -ne 0 ]
         echo -e "OK     NVMe Module is installed and available on your instance"
         echo -e "------------------------------------------------"
 
-        # Calling function to check if NVMe module is loaded in initramfs
-        check_NVMe_initrd
-
     fi
 else
     # NVMe modules is built into the kernel
@@ -254,8 +333,16 @@ else
     echo -e "------------------------------------------------"
     echo -e "OK     NVMe Module is installed and available on your VM"
     echo -e "------------------------------------------------"
+
 fi
 
+# Calling function to check if NVMe module is loaded in initramfs
+check_NVMe_initrd
+
+# Calling function to check GRUB nvme timeout parameters
+check_grub_nvme_timeout
+
+# check fstab for device names
 check_fstab
 
 echo -e "------------------------------------------------"
