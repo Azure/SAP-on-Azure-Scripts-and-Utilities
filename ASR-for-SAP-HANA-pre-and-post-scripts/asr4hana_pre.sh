6f6a3c25330518d1aa494da53119cf856b82ff5c
Ben Trinh <bentrin+microsoft@microsoft.com>
2025-05-27 17:13:53 -0700

    handled $sid variable

--- ASR-for-SAP-HANA-pre-and-post-scripts/asr4hana_pre.sh
+++ ASR-for-SAP-HANA-pre-and-post-scripts/asr4hana_pre.sh
@@ -1,11 +1,18 @@
 #### asr4hana_pre.sh
 
-# The handle `<sid>` is a placeholder for your SAP HANA system identifier.  You will need the hdbuserstore SYSTEMDB key created already for user `<sid>`adm to access HANA and execute the hdbsql command.  
+#!/bin/bash
+set -x
+# Define the path to the DEFAULT.PFL file
+PROFILE_PATH="/usr/sap/*/SYS/profile/DEFAULT.PFL"
 
-    #!/bin/bash
-    # pre-script to prepare HANA for data snapshots
-    # replace the `<sid>` with your 3-letter system identifier
-        echo "Starting pre script as user root"
-        sudo su - `<sid>`adm -c "hdbsql -U SYSTEMDB \"BACKUP DATA FOR FULL SYSTEM CREATE SNAPSHOT COMMENT 'internal DB snapshot';\""
+for profile in $(ls $PROFILE_PATH 2>/dev/null); do
+  dbname=$(grep -E "^dbs/hdb/dbname" "$profile" | awk -F'=' '{print $2}'| tr '[:upper:]' '[:lower:]' | tr -d ' ')
+  if [ -n "$dbname" ]; then
+    echo "dbname found in $profile: $dbname"
+    break # exit once the dbname is found
+  fi
+done
+   # Use the SID variable in the hdbsql command
+    sudo su - "${dbname}adm" -c "hdbsql -U SYSTEMDB \"BACKUP DATA FOR FULL SYSTEM CREATE SNAPSHOT COMMENT 'internal DB snapshot';\""
     # hand over process to ASR at this point
     exit 0
